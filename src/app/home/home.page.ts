dd2145147d25131723ea600e8f8e2b7a07051b5a
Jocsan JM. Moncada <jmoncada01@istmania.hn>
2023-05-22 11:33:32 -0600

    gps cambios

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Camera, CameraResultType, CameraSource } from '@capacitor/camera';
 import { Geolocation, Position } from '@capacitor/geolocation';
 import { Share } from '@capacitor/share';
@@ -8,11 +8,27 @@ import { Share } from '@capacitor/share';
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
   myImage: any;
   position?: any;
 
+  cambioDistancias!: number;
+
   constructor() {}
+  ngOnInit(): void {
+    const latitudPuntoA = 40.7128; // Latitud del punto A en grados
+    const longitudPuntoA = -74.006; // Longitud del punto A en grados
+    const latitudPuntoB = 40.7306; // Latitud del punto B en grados
+    const longitudPuntoB = -73.9352; // Longitud del punto B en grados
+    const distancia = this.calcularDistancia(
+      latitudPuntoA,
+      longitudPuntoA,
+      latitudPuntoB,
+      longitudPuntoB
+    );
+    this.cambioDistancias = distancia;
+    console.log('Diferencia en metros:', distancia.toFixed(2), 'metros');
+  }
 
   async takePicture() {
     const image = await Camera.getPhoto({
@@ -39,5 +55,25 @@ export class HomePage {
         ${this.position.coords.longitude}`,
       url: 'http://ionicacademy.com/',
     });
+  }
+  //? calculo de distancias
+  calcularDistancia(lat1: number, lon1: number, lat2: number, lon2: number) {
+    const radioTierra = 6371000; // Radio de la Tierra en metros
+    const dLat = this.toRad(lat2 - lat1);
+    const dLon = this.toRad(lon2 - lon1);
+    const a =
+      Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+      Math.cos(this.toRad(lat1)) *
+        Math.cos(this.toRad(lat2)) *
+        Math.sin(dLon / 2) *
+        Math.sin(dLon / 2);
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    const distancia = radioTierra * c;
+
+    return distancia;
+  }
+
+  toRad(grados: any) {
+    return (grados * Math.PI) / 180;
   }
 }
