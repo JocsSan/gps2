30ce32b1e08adb8ed42b8038372ace55865b767f
Jocsan JM. Moncada <jmoncada01@istmania.hn>
2023-06-21 14:58:46 -0600

    parece que ya

--- src/app/components/mapbox-map/mapbox-map.component.ts
+++ src/app/components/mapbox-map/mapbox-map.component.ts
@@ -131,67 +131,32 @@ export class MapboxMapComponent implements OnInit, AfterViewInit, OnChanges {
     console.log(source);
 
     if (source) {
-      this.map.on('load', () => {
-        // Add an image to use as a custom marker
-        this.map.loadImage(
-          'https://docs.mapbox.com/mapbox-gl-js/assets/custom_marker.png',
-          (error: any, image: any) => {
-            if (error) throw error;
-            this.map.addImage('custom-marker', image);
-            // Add a GeoJSON source with 2 points
-            this.map.addSource('points', {
-              type: 'geojson',
-              data: {
-                type: 'FeatureCollection',
-                features: [
-                  {
-                    // feature for Mapbox DC
-                    type: 'Feature',
-                    geometry: {
-                      type: 'Point',
-                      coordinates: [
-                        this.destiniyPoint.lng,
-                        this.destiniyPoint.lat,
-                      ],
-                    },
-                    properties: {
-                      title: 'Mapbox DC',
-                    },
-                  },
-                  {
-                    // feature for Mapbox DC
-                    type: 'Feature',
-                    geometry: {
-                      type: 'Point',
-                      coordinates: [
-                        this.currentPoint.lng,
-                        this.currentPoint.lat,
-                      ],
-                    },
-                    properties: {
-                      title: 'Mapbox Current Point',
-                    },
-                  },
-                ],
-              },
-            });
-
-            // Add a symbol layer
-            this.map.addLayer({
-              id: 'points',
-              type: 'symbol',
-              source: 'points',
-              layout: {
-                'icon-image': 'custom-marker',
-                // get the title name from the source's "title" property
-                'text-field': ['get', 'title'],
-                'text-font': ['Open Sans Semibold', 'Arial Unicode MS Bold'],
-                'text-offset': [0, 1.25],
-                'text-anchor': 'top',
-              },
-            });
-          }
-        );
+      this.map.getSource('points').setData({
+        type: 'FeatureCollection',
+        features: [
+          {
+            // feature for Mapbox DC
+            type: 'Feature',
+            geometry: {
+              type: 'Point',
+              coordinates: [this.destiniyPoint.lng, this.destiniyPoint.lat],
+            },
+            properties: {
+              title: 'Mapbox DC',
+            },
+          },
+          {
+            // feature for Mapbox DC
+            type: 'Feature',
+            geometry: {
+              type: 'Point',
+              coordinates: [this.currentPoint.lng, this.currentPoint.lat],
+            },
+            properties: {
+              title: `Mapbox Current Point new ${this.currentPoint.lng} & ${this.currentPoint.lat}`,
+            },
+          },
+        ],
       });
     }
   }
