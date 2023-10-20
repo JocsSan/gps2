81b82fadc3f8a4041a664df6a27b75226384d332
Jocsan JM. Moncada <jmoncada01@istmania.hn>
2023-10-20 11:01:17 -0600

    cambiando a una libreria porque no me gusta escupir codigo y menos uno que no entiendo

--- src/app/standAlone-components/draw/draw.component.ts
+++ src/app/standAlone-components/draw/draw.component.ts
@@ -1,6 +1,5 @@
 import { CommonModule } from '@angular/common';
 import {
-  AfterViewInit,
   Component,
   ElementRef,
   HostListener,
@@ -9,6 +8,7 @@ import {
 } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
+import SignaturePad from 'signature_pad';
 
 @Component({
   selector: 'app-draw',
@@ -17,105 +17,18 @@ import { IonicModule } from '@ionic/angular';
   styleUrls: ['./draw.component.scss'],
   imports: [CommonModule, IonicModule, FormsModule],
 })
-export class DrawComponent implements OnInit, AfterViewInit {
-  @ViewChild('canvasRef', { static: false }) canvasRef!: ElementRef;
-
-  public width: number = 500;
-  public height: number = 450;
-
-  private cx!: CanvasRenderingContext2D;
-
-  private points: Array<any> = [];
-
-  public isAvailabe: boolean = false;
-
-  @HostListener('document:mousemove', ['$event'])
-  onMouseMove = (e: any) => {
-    if (e.target.id === 'canvasId' && this.isAvailabe) {
-      this.write(e);
-    }
-  };
-
-  @HostListener('click', ['$event'])
-  onClick = (e: any) => {
-    if (e.target.id === 'canvasId') {
-      this.isAvailabe = !this.isAvailabe;
-    }
-  };
-
-  //raton eventos
-  @HostListener('document:touchmove', ['$event'])
-  onTouchMove = (e: any) => {
-    if (e.target.id === 'canvasId' && this.isAvailabe) {
-      this.write(e);
-    }
-  };
-
-  @HostListener('document:touchend', ['$event'])
-  onTouchEnd = (e: any) => {
-    if (e.target.id === 'canvasId') {
-      this.isAvailabe = !this.isAvailabe;
-    }
-  };
+export class DrawComponent implements OnInit {
+  @ViewChild('canvas', { static: true }) canvas!: ElementRef;
+  sig!: SignaturePad;
 
   constructor() {}
 
-  ngOnInit() {}
-  ngAfterViewInit(): void {
-    this.render();
-  }
-  private render() {
-    const canvasEl = this.canvasRef.nativeElement;
-    this.cx = canvasEl.getContext('2d');
-    canvasEl.width = this.width;
-    canvasEl.height = this.height;
-
-    this.cx.lineWidth = 3;
-    this.cx.lineCap = 'round';
-    this.cx.strokeStyle = '#000';
-  }
-  private write(res: any) {
-    const canvasEl = this.canvasRef.nativeElement;
-    const rect = canvasEl.getBoundingClientRect();
-
-    let touch;
-    if (res.touches) {
-      touch = res.touches[0];
-    } else {
-      touch = res;
-    }
-
-    const prevPos = {
-      x: touch.clientX - rect.left,
-      y: touch.clientY - rect.top,
-    };
-    this.writeSingle(prevPos);
+  ngOnInit() {
+    this.sig = new SignaturePad(this.canvas.nativeElement);
   }
 
-  private writeSingle(prevPos: any, emit: boolean = true) {
-    this.points.push(prevPos);
-    if (this.points.length > 3) {
-      const prevPost = this.points[this.points.length - 1];
-      const currentPost = this.points[this.points.length - 2];
-
-      this.drawOnCanvas(prevPost, currentPost);
-      if (emit) {
-        //this.socketWebService.emitEvent({ prevPost })
-      }
-    }
+  @HostListener('window:keydown.enter', ['$event'])
+  clear() {
+    this.sig.clear();
   }
-  private drawOnCanvas(prevPos: any, currentPost: any) {
-    if (!this.cx) return;
-    this.cx.beginPath();
-
-    if (prevPos) {
-      this.cx.moveTo(prevPos.x, prevPos.y);
-      this.cx.lineTo(currentPost.x, currentPost.y);
-      this.cx.stroke();
-    }
-  }
-  public clearZone = () => {
-    this.points = [];
-    this.cx.clearRect(0, 0, this.width, this.height);
-  };
 }
