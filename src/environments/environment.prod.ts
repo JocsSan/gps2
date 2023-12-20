03e21a963d97299e27116fcf6b350eb4276fb07b
Jocsan JM. Moncada <jmoncada01@istmania.hn>
2023-12-20 11:57:16 -0600

    se agreagaron jwt a algunas rutas y guards pre prod para pruebas

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   // geotUrl: 'tcp://2.tcp.ngrok.io:19480/',
   geotUrl: 'https://53d0-190-4-28-69.ngrok-free.app/',
-  version: '0.0.1.6',
-  changeDistance: 1,
+  version: '0.0.1.6.0001',
+  changeDistance: 50,
   rechargeMap: 200,
 };
