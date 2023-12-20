0e1069411df8a560f458b3bba3bfe53e5c57721d
Jocsan JM. Moncada <jmoncada01@istmania.hn>
2023-12-20 16:42:26 -0600

    se agreagaron jwt a algunas rutas y guards pre prod para pruebas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { StorageService } from './services/storage.service';
 import { NetworkService } from './services/net-work.service';
 import { SwUpdate } from '@angular/service-worker';
 import { concat, first, interval } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-root',
@@ -16,25 +17,31 @@ export class AppComponent {
     public updates: SwUpdate,
     private appRef: ApplicationRef
   ) {
-    // Permite a la aplicación estabilizarse antes de comprobar las actualizaciones
-    const appIsStable$ = appRef.isStable.pipe(
-      first((isStable) => isStable === true)
-    );
-    const everySixHours$ = interval(6 * 60 * 60 * 1000);
-    const everySixHoursOnceAppIsStable$ = concat(appIsStable$, everySixHours$);
+    // Comprueba si los Service Workers están disponibles
+    if ('serviceWorker' in navigator && environment.production) {
+      // Permite a la aplicación estabilizarse antes de comprobar las actualizaciones
+      const appIsStable$ = appRef.isStable.pipe(
+        first((isStable) => isStable === true)
+      );
+      const everySixHours$ = interval(6 * 60 * 60 * 1000);
+      const everySixHoursOnceAppIsStable$ = concat(
+        appIsStable$,
+        everySixHours$
+      );
 
-    everySixHoursOnceAppIsStable$.subscribe(() => updates.checkForUpdate());
+      everySixHoursOnceAppIsStable$.subscribe(() => updates.checkForUpdate());
 
-    updates.available.subscribe((event) => {
-      console.log('current version is', event.current);
-      console.log('available version is', event.available);
-      updates.activateUpdate().then(() => this.updateApp());
-    });
+      updates.available.subscribe((event) => {
+        console.log('current version is', event.current);
+        console.log('available version is', event.available);
+        updates.activateUpdate().then(() => this.updateApp());
+      });
 
-    updates.activated.subscribe((event) => {
-      console.log('old version was', event.previous);
-      console.log('new version is', event.current);
-    });
+      updates.activated.subscribe((event) => {
+        console.log('old version was', event.previous);
+        console.log('new version is', event.current);
+      });
+    }
   }
 
   updateApp() {
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   // geotUrl: 'tcp://2.tcp.ngrok.io:19480/',
   geotUrl: 'https://53d0-190-4-28-69.ngrok-free.app/',
-  version: '0.0.1.6.0001x.2',
+  version: '0.0.1.6.0001x.21',
   changeDistance: 50,
   rechargeMap: 200,
 };
