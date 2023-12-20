28b448903ed32c4a667a61ae7a22d7952b33a69d
Jocsan JM. Moncada <jmoncada01@istmania.hn>
2023-12-20 13:16:39 -0600

    se agreagaron jwt a algunas rutas y guards pre prod para pruebas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { ApplicationRef, Component } from '@angular/core';
 import { StorageService } from './services/storage.service';
 import { NetworkService } from './services/net-work.service';
 import { SwUpdate } from '@angular/service-worker';
+import { concat, first, interval } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -12,19 +13,27 @@ export class AppComponent {
   constructor(
     private storage$: StorageService,
     private network$: NetworkService,
-    public updates: SwUpdate
+    public updates: SwUpdate,
+    private appRef: ApplicationRef
   ) {
+    // Permite a la aplicación estabilizarse antes de comprobar las actualizaciones
+    const appIsStable$ = appRef.isStable.pipe(
+      first((isStable) => isStable === true)
+    );
+    const everySixHours$ = interval(6 * 60 * 60 * 1000);
+    const everySixHoursOnceAppIsStable$ = concat(appIsStable$, everySixHours$);
+
+    everySixHoursOnceAppIsStable$.subscribe(() => updates.checkForUpdate());
+
     updates.available.subscribe((event) => {
       console.log('current version is', event.current);
       console.log('available version is', event.available);
+      updates.activateUpdate().then(() => this.updateApp());
     });
+
     updates.activated.subscribe((event) => {
       console.log('old version was', event.previous);
       console.log('new version is', event.current);
-    });
-
-    updates.available.subscribe((event) => {
-      updates.activateUpdate().then(() => this.updateApp());
     });
   }
 
