5981a08336184d53f258c4c95a055cb09d5a7998
Jocsan JM. Moncada <jmoncada01@istmania.hn>
2024-01-16 12:13:28 -0600

    style: :art: Cambis en formulario y endpoint

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   //geotUrl: 'http://localhost:3000/',
   geotUrl: 'https://6981-190-4-28-69.ngrok-free.app/',
-  version: '0.0.1.6.0001x.219000.00090.99',
+  version: '0.0.1.6.0001x.219000.00090.99.soy yo de nuevo',
   changeDistance: 50,
   rechargeMap: 200,
 };
