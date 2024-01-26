762339f7f693c6b67ca65bfe38adced006cd4ee3
Jocsan JM. Moncada <jmoncada01@istmania.hn>
2024-01-26 09:32:27 -0600

    cambios en toma de pedidos api

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,9 @@
 export const environment = {
   production: true,
   //geotUrl: 'http://localhost:3000/',
-  geotUrl: 'https://cfbf-190-4-28-69.ngrok-free.app/',
-  version: '0.0.1.6.prev.prod.ts.1 "logger fixes 3"',
+  geotUrl: 'https://9a3d-190-4-28-69.ngrok-free.app/',
+  //geotUrl: 'https://cfbf-190-4-28-69.ngrok-free.app/',
+  version: '0.0.1.6.prev.prod.ts.1 "logger fixes 3 new api"',
   changeDistance: 50,
   rechargeMap: 200,
 };
