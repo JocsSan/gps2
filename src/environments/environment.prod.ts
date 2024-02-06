9fcc46164187971e64083075580828029cc14557
Jocsan JM. Moncada <jmoncada01@istmania.hn>
2024-02-06 08:43:04 -0600

    cambio de api

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   //geotUrl: 'http://localhost:3000/',
-  geotUrl: 'https://79e7-190-4-28-69.ngrok-free.app/',
+  geotUrl: 'https://f562-190-4-28-69.ngrok-free.app/',
   //geotUrl: 'https://cfbf-190-4-28-69.ngrok-free.app/',
   version: '0.0.1.6.prev.prod.ts.1 "logger fixes 3 new api" *.2',
   changeDistance: 50,
